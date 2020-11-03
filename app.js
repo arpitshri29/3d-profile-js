ee4b6beac6a2d9e7f9f4d34a19924679efc03793
Arpit <arpit@interfacem.com>
2020-11-04 02:04:33 +0530

    Added 3d effect on mouse hover

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const container = document.querySelector('.container');
 const profile = document.getElementById('profile');
+const name = document.getElementById('name');
+const image = document.getElementById('image');
+const description = document.getElementById('description');
+
 
 // Animation
 container.addEventListener("mousemove", e => {
@@ -8,13 +12,29 @@ container.addEventListener("mousemove", e => {
     profile.style.transform = `rotateX(${yAxis}deg) rotateY(${xAxis}deg)`
 });
 
-//
+// Animate on hover
 container.addEventListener("mouseenter", e =>{
    profile.style.transition = 'none';
+
+   name.style.transform =  'translateZ(150px)';
+   name.style.textShadow = '0 2px 3px rgba(0,0,0,.3)';
+
+   image.style.transform =  'translateZ(150px)';
+
+   description.style.transform =  'translateZ(150px)';
+   description.style.textShadow = '0 2px 3px rgba(0,0,0,.3)';
 });
 
 // Reset profile to default
 container.addEventListener("mouseleave", e => {
     profile.style.transition = 'all 0.5s ease';
-   profile.style.transform = `rotateX(0deg) rotateY(0deg)`
+    profile.style.transform = `rotateX(0deg) rotateY(0deg)`;
+
+    name.style.transform =  'translateZ(0px)';
+    name.style.textShadow = 'none';
+
+    image.style.transform =  'translateZ(0px)';
+
+    description.style.transform =  'translateZ(0px)';
+    description.style.textShadow = 'none';
 });
